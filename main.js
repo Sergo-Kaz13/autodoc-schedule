d13c4a5dde513e5e1d2e7763513f2ef1a971173c
Sergo_Kaz <wwe619dx@ukr.net>
2024-12-15 22:17:14 +0100

    add function createSchedule

--- main.js
+++ main.js
@@ -1,1 +1,83 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const schedule = localStorage.getItem("schedule");
+
+  if (!schedule) {
+    createSchedule();
+  }
+
+  console.log(schedule);
+});
+
+const days = [
+  "понеділок",
+  "вівторок",
+  "середа",
+  "четверг",
+  "пятниця",
+  "субота",
+  "неділя",
+];
+
+function createSchedule() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  let numDayWeek = new Date(`${year}-${month}-01`).getDay() - 1;
+  // const numDaysMonth = daysInMonth(month);
+  const numDaysMonth = daysInMonth(month);
+
+  numDayWeek = numDayWeek < 0 ? (numDayWeek = 6) : (numDayWeek = numDayWeek);
+
+  let daysSchedule = numDaysMonth + numDayWeek;
+
+  while (!(daysSchedule % 7 === 0)) {
+    daysSchedule++;
+  }
+
+  const schedule = document.querySelector(".schedule");
+  schedule.classList.add("schedule");
+
+  let startDayDate = 1;
+
+  for (let index = 0; index < daysSchedule; index++) {
+    const scheduleItem = document.createElement("div");
+    const span = document.createElement("span");
+    if (index >= numDayWeek && index <= numDaysMonth + numDayWeek - 1) {
+      span.textContent = startDayDate;
+      startDayDate++;
+      scheduleItem.classList.add("scheduleItemWork");
+    }
+    scheduleItem.classList.add("scheduleItem");
+    if (
+      (index >= numDayWeek && index === 5) ||
+      index === 6 ||
+      index === 12 ||
+      index === 13 ||
+      index === 19 ||
+      index === 20 ||
+      index === 26 ||
+      index === 27 ||
+      index === 33 ||
+      index === 34
+    ) {
+      scheduleItem.classList.add("scheduleItemDayOff");
+    }
+    scheduleItem.append(span);
+    schedule.append(scheduleItem);
+  }
+
+  console.log(["schedule"], schedule);
+}
+
+function daysInMonth(month, year) {
+  month = month + 1;
+  if (month > 12) month = month - 12;
+  year = year != undefined ? year : new Date().getFullYear();
+  let d = new Date(year + "-" + month + "-01");
+  d.setDate(0);
+  d = d.getDate();
+
+  return d;
+}
