8aa6bbda61db4dc04b3118c027928a2972f1c481
Sergo_Kaz <wwe619dx@ukr.net>
2025-04-06 21:50:48 +0200

    add pwa setting

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "schedule-app-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/main.js",
+  "/icon-192.png",
+];
+
+// Кешування файлів при встановленні
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Обробка запитів
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
