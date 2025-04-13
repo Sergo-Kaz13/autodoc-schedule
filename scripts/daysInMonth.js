092113160dc6f9032f700bcc42b7a2759d2d4f43
Sergo_Kaz <wwe619dx@ukr.net>
2025-04-13 20:42:01 +0200

    daysInMonth edit

--- scripts/daysInMonth.js
+++ scripts/daysInMonth.js
@@ -1,10 +1,15 @@
 export function daysInMonth(month, year) {
-  month = month + 1;
-  if (month > 12) month = month - 12;
-  year = year != undefined ? year : new Date().getFullYear();
-  let d = new Date(year + "-" + month + "-01");
-  d.setDate(0);
-  d = d.getDate();
+  // month = month + 1;
+  // if (month > 12) month = month - 12;
+  // year = year != undefined ? year : new Date().getFullYear();
+  // let d = new Date(year + "-" + month + "-01");
+  // d.setDate(0);
+  // d = d.getDate();
 
-  return d;
+  // return d;
+
+  year = year !== undefined ? year : new Date().getFullYear();
+  const date = new Date(year, month, 0);
+
+  return date.getDate();
 }
